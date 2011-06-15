fbd9a26ef452c490584751f9cf00f12de7a44db9
Rodrigo.Ruz.V <Rodrigo.Ruz.V@b819dc04-cec3-4c4a-4a66-575601f9b720>
2011-06-15 02:14:46 +0000

    Added extra files

--- trunk/WmiDelphiCodeCreator.dpr
+++ trunk/WmiDelphiCodeCreator.dpr
@@ -2,20 +2,19 @@ program WmiDelphiCodeCreator;
 
 uses
   Forms,
-  MainWmiDelphiC in 'MainWmiDelphiC.pas' {FrmMain},
+  Main in 'Main.pas' {FrmMain},
   uWmiTree in 'Units\uWmiTree.pas' {FrmWMITree},
   uWmi_Metadata in '..\..\2007\WmiMetadata\uWmi_Metadata.pas',
   uDelphiSyntax in 'Units\uDelphiSyntax.pas',
   uListView_Helper in 'Units\uListView_Helper.pas',
-  Wmi_About in 'Units\Wmi_About.pas' {FrmAbout},
+  uWmi_About in 'Units\uWmi_About.pas' {FrmAbout},
   uWmiDelphiCode in 'Units\uWmiDelphiCode.pas',
-  AsyncCalls in 'C:\Delphi\Librerias\AsyncCalls\AsyncCalls.pas',
   uDelphiIDE in 'Units\uDelphiIDE.pas',
   uDelphiPrismHelper in 'Units\uDelphiPrismHelper.pas',
   uDelphiPrismIDE in 'Units\uDelphiPrismIDE.pas',
   uLazarusIDE in 'Units\uLazarusIDE.pas',
   uSelectCompilerVersion in 'Units\uSelectCompilerVersion.pas' {FrmSelCompilerVer},
-  Wmi_ViewPropsValues in 'Units\Wmi_ViewPropsValues.pas' {FrmWmiVwProps},
+  uWmi_ViewPropsValues in 'Units\uWmi_ViewPropsValues.pas' {FrmWmiVwProps},
   uRegistry in 'Units\uRegistry.pas',
   uWmiDatabase in 'Units\uWmiDatabase.pas' {FrmWmiDatabase},
   uCustomImageDrawHook in 'Units\uCustomImageDrawHook.pas',
@@ -26,7 +25,8 @@ uses
   uDelphiVersions in 'Units\uDelphiVersions.pas',
   uWmiGenCode in 'Units\uWmiGenCode.pas',
   uWmiOxygenCode in 'Units\uWmiOxygenCode.pas',
-  uWmiFPCCode in 'Units\uWmiFPCCode.pas';
+  uWmiFPCCode in 'Units\uWmiFPCCode.pas',
+  AsyncCalls in 'Units\AsyncCalls.pas';
 
 {$R *.res}
 
